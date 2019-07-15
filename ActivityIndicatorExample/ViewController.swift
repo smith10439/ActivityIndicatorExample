7095cf53f1c982242f54e52e71e335df6323abc1
Sam Smith <smith.10439@osu.edu>
2019-07-15 09:27:17 -0400

    Initial Commit

---
+++ ActivityIndicatorExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivityIndicatorExample
+//
+//  Created by Smith, Sam on 7/15/19.
+//  Copyright © 2019 Smith, Sam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
